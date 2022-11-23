be5111d183b22cbc008ce246e81cc95df37b74e3
chriszack3 <czack@chrispc.localdomain>
2022-11-23 15:15:54 -0500

    time recored

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ const runScraper = async (sub) => {
     const divs = divArr.map(div => {
         const author = div.querySelector('p.tagline > a.author').innerText;
         const body = div.querySelector('div.md').innerText;
-        const time = div.querySelector('time.live-timestamp').getAttribute('datetime');
+        const postedAt = div.querySelector('time.live-timestamp').getAttribute('datetime');
+        const timeScraped = new Date().toISOString();
         const fullThread = div.querySelector('a.bylink').getAttribute('href');
         const scoreHidden = div.querySelector('span.score-hidden').title;
         const dislikes = div.querySelector('span.dislikes');
@@ -18,7 +19,7 @@ const runScraper = async (sub) => {
         const score = div.querySelector('span.score.unvoted');
         const commentId = div.getAttribute('data-fullname');
 
-        return { author, body, time, fullThread, scoreHidden, dislikes, likes, score, commentId };
+        return { author, body, postedAt, timeScraped, fullThread, scoreHidden, dislikes, likes, score, commentId };
     })
     return divs;
   })

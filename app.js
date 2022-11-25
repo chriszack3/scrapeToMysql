5b35b3896b5e5981b3aa5cfdb21e4198811c823f
chriszack3 <czack@chrispc.localdomain>
2022-11-25 17:31:21 -0500

    refactored loops for speed, n-1

--- app.js
+++ app.js
@@ -1,34 +1,38 @@
 const puppeteer = require('puppeteer');
 
-const getFirstOfMonth = () => {
-    
-}
-
 const runScraper = async (sub) => {
   const browser = await puppeteer.launch({ args: ['--no-sandbox'] })
-  const page = await browser.newPage()
-  await page.goto(`https://old.reddit.com/r/${sub || 'stocks'}/comments/`);
 
-  // Get the "viewport" of the page, as reported by the page.
-  const data = await page.$$eval('div.comment', divArr => {
-    const divs = divArr.map(div => {
-        const author = div.querySelector('p.tagline > a.author').innerText;
-        const body = div.querySelector('div.md').innerText;
-        const postedAt = div.querySelector('time.live-timestamp').getAttribute('datetime');
+  let comments = []
+  while (comments.length < 49) {
+    const page = await browser.newPage()
+    comments.length > 0 ? await page.goto(`https://old.reddit.com/r/${sub || 'stocks'}/comments/?count=${comments.length}&after=${comments?.[comments.length - 1]?.commentId}`) : await page.goto(`https://old.reddit.com/r/${sub || 'stocks'}/comments/`);
+    // Get the "viewport" of the page, as reported by the page.
+    const data = await page.$$eval('div.comment', (divArr) => {
+      
+      return divArr.map((comment, index) => {
+        const author = comment.querySelector('p.tagline > a.author').innerText;
+        const body = comment.querySelector('div.md').innerText;
+        const postedAt = comment.querySelector('time.live-timestamp').getAttribute('datetime');
         const timeScraped = new Date().toISOString();
-        const fullThread = div.querySelector('a.bylink').getAttribute('href');
-        const scoreHidden = div.querySelector('span.score-hidden').title;
-        const dislikes = div.querySelector('span.dislikes');
-        const likes = div.querySelector('span.likes');
-        const score = div.querySelector('span.score.unvoted');
-        const commentId = div.getAttribute('data-fullname');
-
+        const fullThread = comment.querySelector('a.bylink').getAttribute('href');
+        const scoreHidden = comment.querySelector('span.score-hidden').title;
+        const dislikes = comment.querySelector('span.dislikes');
+        const likes = comment.querySelector('span.likes');
+        const score = comment.querySelector('span.score.unvoted');
+        const commentId = comment.getAttribute('data-fullname');
         return { author, body, postedAt, timeScraped, fullThread, scoreHidden, dislikes, likes, score, commentId };
-    })
-    return divs;
-  })
+      })
+    });
+
+
+    comments.push(data);
+    console.log(comments)
+  }
+
+
   await browser.close()
-  return data
+  return comments;
 };
 
-runScraper().then(data => console.log(data));
+runScraper()
